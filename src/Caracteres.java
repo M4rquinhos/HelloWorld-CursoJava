5054268931eefe911ecdbae4306051054a33e3ed
Marco Carrillo <marco.37099p@gmail.com>
2023-02-10 15:25:56 -0600

    Primitivo char y caracter

---
+++ src/Caracteres.java
@@ -0,0 +1,18 @@
+public class Caracteres {
+    public static void main(String[] args) {
+        char caracter = '\u0040';
+        char decimal = 64;
+        System.out.println("caracter = " + caracter);
+        System.out.println("decimal = " + decimal);
+        System.out.println("¿ caracter = decimal ?: " + (caracter == decimal));
+        
+        char simbolo = '@';
+        System.out.println("simbolo = " + simbolo);
+        System.out.println("¿ caracter = simbolo ?: " + (caracter == simbolo));
+
+        System.out.println("Tipo char corresponde en byte a: " + Character.BYTES);
+        System.out.println("Tipo char corresponde en bites a: " + Character.SIZE);
+        System.out.println("Valor mínimo de un char: " + Character.MIN_VALUE);
+        System.out.println("Valor máximo de un char: " + Character.MAX_VALUE);
+    }
+}
