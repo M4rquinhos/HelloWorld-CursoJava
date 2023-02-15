41889797bc0b97b405f60bb2701f287ece8e6fa9
Marco Carrillo <marco.37099p@gmail.com>
2023-02-15 14:37:31 -0600

    Validar un string

---
+++ src/EjemploStringValidar.java
@@ -0,0 +1,10 @@
+public class EjemploStringValidar {
+    public static void main(String[] args) {
+
+        String curso =  null;
+
+        boolean esNull = curso == null;
+
+        System.out.println("esNull = " + esNull);
+    }
+}
